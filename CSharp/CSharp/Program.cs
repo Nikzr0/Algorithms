eb7b2bc3874a25e200de588f84106508aa21c530
Nikzr0 <npepelov@gmail.com>
2024-06-11 09:08:20 +0300

    Initial commit

---
+++ CSharp/CSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            List<char> n = new List<char>(input);
+
+            if (input.Length == 1)
+            {
+                Console.WriteLine(input);
+                return;
+            }
+
+            ulong first = ulong.Parse(n[0].ToString());
+            ulong second = ulong.Parse(n[1].ToString());
+
+            if (first < second || n.Distinct().Count() == 1)
+            {
+                uint result = (uint)(((input.Length - 1) * 9) + (uint)first);
+                Console.WriteLine(result);
+            }
+            else
+            {
+                uint result = (uint)(((input.Length - 1) * 9) + (uint)first - 1);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
