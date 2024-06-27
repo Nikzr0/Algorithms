aa5e85db492e10cab59067828105c9e210404de2
Nikzr0 <npepelov@gmail.com>
2024-06-27 16:21:49 +0300

    Description and solution for the next exercises

---
+++ CPlusPlus/OddNumbers/OddNumbers.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n; cin >> n;
+	vector<string> oddDigits = { "1", "3", "5", "7", "9" };
+
+
+	int index = 0;
+	string current;
+
+	while (true) {
+		current = oddDigits[index];
+		++index;
+
+		oddDigits.push_back(current + "1");
+		oddDigits.push_back(current + "3");
+		oddDigits.push_back(current + "5");
+		oddDigits.push_back(current + "7");
+		oddDigits.push_back(current + "9");
+
+		if (oddDigits.size() >= n)
+		{
+			cout << oddDigits[n - 1] << endl;
+			return 0;
+		}
+	}
+}
+//Task
+//Подреждаме в растящ ред целите числа, които имат само нечетни цифри : 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 31, ...
+//
+//Напишете програма odd, която намира n - тото число от тази редица.Броенето започваме от 1, така че първото търсенo число е 1.
+//
+//Input Format
+//
+//Едно цяло положително число, задаващо стойността на n
+//
+//Constraints
+//
+//0 < n < 100 000
+//
+//	Output Format
+//
+//	Едно цяло число, равно на n - тото число от описаната редица
---
+++ CPlusPlus/Stamps/Stamps.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main()
+{
+	long long n, m, p, q;
+	cin >> n >> m >> p >> q;
+
+    m = m * 2;
+
+    int output = 0;
+    long long sideCapacity = p * q;
+
+	map<long long, long long> stamps;
+	
+    int temp;
+    for (int i = 0; i < n; ++i) {
+         cin >> temp;
+
+         if (stamps.find(temp) != stamps.end())
+         {
+			stamps[temp]++;
+		}
+         else
+         {
+             stamps[temp] = 1;
+         }
+    }
+
+    output = stamps.size();
+    for (auto it = stamps.begin(); it != stamps.end(); ++it) {
+     
+        if (it -> second > sideCapacity)
+        {
+            int count = it->second;
+            while (count > sideCapacity)
+            {
+                output++;
+                count -= sideCapacity;
+            }
+        }
+    }
+
+
+    if (output % m == 0) {
+        cout << output / m << endl;
+    }
+    else {
+        cout << output / m + 1 << endl;
+    }
+
+  
+
+}
+
+//Task
+//Днес Аялайя ще подрежда колекцията си от n пощенски марки в класьори.Класьорите имат по m картонени листа за марки, на всяка страница от листа са оформени по p реда с джобчета за марки, а на всеки ред се събират по q марки.На всяка страница Аялайя подрежда само марки с еднакъв номинал*, което понякога довежда до частично запълнени страници – просто няма достатъчно марки с този номинал, за да запълнят страницата.
+//
+//Input Format
+//
+//Програмата прочита от стандартния вход n, m, p и q.След това прочита номиналите на n - те марки.
+//
+//Constraints
+//
+//0 < n ≤ 10 ^ 6, 0 < m ≤ 40, 1 < p, q ≤ 10; номиналите на марките в колекцията са цели положителни числа, не по - големи от 1000.
+//
+//    Output Format
+//
+//    Програмата да отпечата на един ред на стандартния изход колко най - малко класьора ще са необходими за подреждане на колекцията, като се спазва описаното правило за подредба.
